f5cf7b60f9080439006ea587962cd2a6ee0b59e8
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2020-12-05 16:54:36 -0500

    Initial commit

---
+++ Assets/Resources/Scripts/DelegateManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateManager
+{
+    // Start is called before the first frame update
+    #region singleton
+
+    private static DelegateManager instance;
+    public static DelegateManager Instance { get { return instance ?? (instance = new DelegateManager()); } }
+    private DelegateManager() { }
+    #endregion
+
+    public delegate void MyDelegate();
+    List<DelegateTimer> delegateTimers;
+
+
+
+    public void Initialize()
+    {
+        delegateTimers = new List<DelegateTimer>();
+    }
+
+
+    public void AddDelegateTimer(MyDelegate toInvoke, float time )
+    {
+        DelegateTimer toAdd = new DelegateTimer(toInvoke,Time.time+ time);
+        delegateTimers.Add(toAdd);
+    }
+    
+
+    public void Refresh()
+    {
+
+         for (int i = delegateTimers.Count - 1; i >=0; i--)
+        {
+
+            if (delegateTimers[i].timeOfInvoke <= Time.time)
+            {
+                delegateTimers[i].delegateToInvoke.Invoke();
+                delegateTimers.RemoveAt(i);
+
+            }
+
+        }
+
+    }
+
+    public void EndGame() 
+    {
+        delegateTimers.Clear();
+    }
+
+    private class DelegateTimer
+    {
+        public float timeOfInvoke;
+        public MyDelegate delegateToInvoke;
+
+        public DelegateTimer( MyDelegate delegateToInvoke, float timeofInvokee)
+        {
+            this.timeOfInvoke = timeofInvokee;
+            this.delegateToInvoke = delegateToInvoke;
+        }
+
+
+    }
+}
---
+++ Assets/Resources/Scripts/MainScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string desiredOutput = "Akki Desired";
+
+
+        DelegateManager.Instance.Initialize();
+
+        string s = desiredOutput;
+        DelegateManager.Instance.AddDelegateTimer(() => {OutputMessage(s); },1.5f);
+        desiredOutput = "Akki changed";
+
+        DelegateManager.Instance.AddDelegateTimer(() => { OutputMessage(desiredOutput); }, 3f); 
+       // DelegateManager.Instance.AddDelegateTimer(MyFunction, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DelegateManager.Instance.Refresh();
+
+    }
+
+
+    public void MyFunction()
+    {
+        Debug.Log("Its Working");
+    }
+
+    public void OutputMessage(string s)
+    {
+        Debug.Log("S : " + s);
+    }
+
+    
+}
